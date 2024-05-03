96b2897ff329447525696b951f37ef5c4a99653d
Oscar Robles <oscarrsvp@gmail.com>
2024-05-02 21:19:25 -0500

    Add User input validation on user signup backend endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
-
-const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { setTokenCookie } = require('../../utils/auth');
+const { validateSignup } = require('../../utils/validation');
 const { User } = require('../../db/models');
 
 const router = express.Router();
 
 // Sign up
-router.post('/', async (req, res) => {
+router.post('/', validateSignup, async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
   const hashedPassword = bcrypt.hashSync(password);
   const user = await User.create({ firstName, lastName, email, hashedPassword });
--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -28,7 +28,30 @@ const validateLogin = [
   handleValidationErrors,
 ];
 
+const validateSignup = [
+  check('email')
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage('Please provide a valid email.'),
+  check('firstName')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 3 })
+    .withMessage('Please provide a first name with at least 3 characters.'),
+  check('firstName').not().isEmail().withMessage('Username cannot be an email.'),
+  check('lastName')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 3 })
+    .withMessage('Please provide a last name with at least 3 characters.'),
+  check('lastName').not().isEmail().withMessage('Username cannot be an email.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage('Password must be 6 characters or more.'),
+  handleValidationErrors,
+];
+
 module.exports = {
   handleValidationErrors,
   validateLogin,
+  validateSignup,
 };
