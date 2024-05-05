406fc414edc01ac852ea7bbe37b092a3bb0625a1
Oscar Robles <oscarrsvp@gmail.com>
2024-05-05 15:16:52 -0500

    Added Login Function & Login Component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,40 @@
+import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import LoginForm from './components/LoginForm';
+import { restoreUser } from './store/sessionSlice';
+
+function Layout() {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    dispatch(restoreUser()).then(() => {
+      setIsLoaded(true);
+    });
+  }, [dispatch]);
+
+  return <>{isLoaded && <Outlet />}</>;
+}
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <h1>Welcome!</h1>,
+      },
+      {
+        path: '/login',
+        element: <LoginForm />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return (
-    <>
-      <h1>Hello</h1>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
