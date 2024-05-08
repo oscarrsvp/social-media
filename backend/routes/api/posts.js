d567a5b62358519b9dc8812a790822a2352be84c
Oscar Robles <oscarrsvp@gmail.com>
2024-05-07 20:59:00 -0500

    Included Comments endpoints

--- backend/routes/api/posts.js
+++ backend/routes/api/posts.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { requireAuth } = require('../../utils/auth');
-const { Post, User } = require('../../db/models');
+const { User, Post, Comment } = require('../../db/models');
 
 router.use(requireAuth);
 
-// Get User's Posts
+// Get all User's Posts
 router.get('/', async (req, res) => {
   const post = await Post.findAll({
     include: User,
@@ -15,6 +15,7 @@ router.get('/', async (req, res) => {
   return res.json(post);
 });
 
+// Create a new Post
 router.post('/', async (req, res) => {
   const userId = req.user.id;
   const { photo, context } = req.body;
@@ -28,6 +29,7 @@ router.post('/', async (req, res) => {
   return res.status(201).json(newPost);
 });
 
+// Get a Post by Id
 router.get('/:postId', async (req, res) => {
   const { postId } = req.params;
   const post = await Post.findByPk(postId);
@@ -37,6 +39,7 @@ router.get('/:postId', async (req, res) => {
   return res.json(post);
 });
 
+// Edit a Post
 router.put('/:postId', async (req, res) => {
   const userId = req.user.id;
   const { postId } = req.params;
@@ -56,6 +59,7 @@ router.put('/:postId', async (req, res) => {
   }
 });
 
+// Delete a Post
 router.delete('/:postId', async (req, res) => {
   const userId = req.user.id;
   const { postId } = req.params;
@@ -69,6 +73,46 @@ router.delete('/:postId', async (req, res) => {
 
     return res.json({ message: `Post deleted` });
   }
+});
+
+// Get all Comments from a Post
+router.get('/:postId/comments', async (req, res) => {
+  const { postId } = req.params;
+
+  const post = await Post.findByPk(postId);
+
+  if (!post) return res.status(404).json({ message: `Post not found` });
+
+  const getCommentsFromPost = await Comment.findAll({
+    include: {
+      model: User,
+      attributes: ['firstName', 'lastName'],
+    },
+    where: {
+      postId,
+    },
+  });
+
+  return res.json({ Comments: getCommentsFromPost });
+});
+
+// Create a new Comment
+router.post('/:postId/comment', async (req, res) => {
+  const userId = req.user.id;
+  const { postId } = req.params;
+  const { context } = req.body;
+
+  const post = await Post.findByPk(postId);
+
+  if (!post) return res.status(404).json({ message: `Post not found` });
+
+  const newComment = await Comment.create({
+    userId,
+    postId,
+    context,
+  });
+
+  return res.status(201).json(newComment);
 });
 
 module.exports = router;
