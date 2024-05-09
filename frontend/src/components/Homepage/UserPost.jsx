1d08ff6d40d6dce470f3d0fc1ed1050eb11c8609
Oscar Robles <oscarrsvp@gmail.com>
2024-05-08 22:56:34 -0500

    Created UserPost component

---
+++ frontend/src/components/Homepage/UserPost.jsx
@@ -0,0 +1,46 @@
+import { useSelector } from 'react-redux';
+import CommentSection from './CommentSection';
+import styles from './Homepage.module.css';
+
+function UserPost() {
+  const post = useSelector((state) => state.posts);
+  const userPost = Object.values(post).reverse();
+
+  const createdAt = (date) => {
+    const getDate = new Date(date);
+    const dates = getDate.toLocaleString('default', {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
+    });
+
+    return dates;
+  };
+
+  if (userPost[0] === null) return null;
+
+  return (
+    <>
+      {userPost.map((user) => (
+        <div className={styles.userPost} key={user.id}>
+          <div>
+            <h2>
+              {user.User.firstName} {user.User.lastName}
+            </h2>
+            <h3></h3>
+            <h4>{createdAt(user.createdAt)}</h4>
+          </div>
+
+          <div>
+            <img src={user.photo} />
+            <p>{user.context}</p>
+          </div>
+
+          <CommentSection post={user} />
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default UserPost;
