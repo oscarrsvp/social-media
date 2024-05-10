6f69757c315bb9621e455160e165e0518924350d
Oscar Robles <oscarrsvp@gmail.com>
2024-05-09 22:40:46 -0500

    Added UpdateComment components

---
+++ frontend/src/components/UpdateInputs/UpdateComment.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { updateComment } from '../../store/commentSlice';
+import { useModal } from '../../context/Modal';
+
+function UpdateComment({ comment }) {
+  const { id } = comment;
+  const [context, setContext] = useState(comment?.context || '');
+  const { closeModal } = useModal();
+
+  const dispatch = useDispatch();
+
+  const handleUpdate = () => {
+    dispatch(updateComment({ id, context }));
+    closeModal();
+  };
+
+  return (
+    <>
+      <div>
+        <input
+          value={context}
+          onChange={(e) => setContext(e.target.value)}
+          placeholder="Whats on your mind?"
+        ></input>
+        <button onClick={handleUpdate}>Update Comment</button>
+      </div>
+    </>
+  );
+}
+
+export default UpdateComment;
