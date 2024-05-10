eaf3842fb20f8f05b082a04869aba24203cc4819
Oscar Robles <oscarrsvp@gmail.com>
2024-05-09 22:47:22 -0500

    Refactor broke code into components

--- frontend/src/components/Homepage/Homepage.jsx
+++ frontend/src/components/Homepage/Homepage.jsx
@@ -3,31 +3,19 @@ import { useSelector, useDispatch } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import { fetchPosts, createPost, deletePost } from '../../store/postSlice';
 import OpenModalButton from '../OpenModalButton/OpenModalButton';
-import UpdatePost from '../UpdatePost/UpdatePost';
+import UpdatePost from '../UpdateInputs/UpdatePost';
+import UserPost from './UserPost';
 import styles from './Homepage.module.css';
 
 function Homepage() {
   const [photo, setPhoto] = useState('');
   const [context, setContext] = useState('');
   const sessionUser = useSelector((state) => state.session.user);
-  const allPosts = useSelector((state) => state.posts);
-  const posts = Object.values(allPosts).reverse();
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchPosts());
   }, [dispatch]);
-
-  const createdAt = (date) => {
-    const getDate = new Date(date);
-    const dates = getDate.toLocaleString('default', {
-      day: 'numeric',
-      month: 'long',
-      year: 'numeric',
-    });
-
-    return dates;
-  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -37,11 +25,8 @@ function Homepage() {
 
   if (!sessionUser) return <Navigate to="/" replace={true} />;
 
-  if (!sessionUser) return <h1>Loading....</h1>;
-
   return (
     <div id={styles.homePage}>
-      {/* <h1>Welcome to the homepage</h1> */}
       <div className={styles.sideBar}>
         SIDEBAR
         <div>News Feed</div>
@@ -69,47 +54,22 @@ function Homepage() {
             </label>
             <button type="submit">Create New Post</button>
           </form>
-          {posts.map((post) => (
-            <div className={styles.userPost} key={`${post?.id}`}>
-              <div>
-                <h2>User&apos;s Img</h2>
-                <h3>
-                  {post?.User?.firstName} {post?.User?.lastName}
-                </h3>
-                <h4>{createdAt(post?.createdAt)}</h4>
-              </div>
-              <div>
-                <img src={post?.photo} />
-                <p>{post?.context}</p>
-                {sessionUser?.id === post?.userId ? (
-                  <>
-                    <button onClick={() => dispatch(deletePost(post?.id))}>
-                      Delete Post
-                    </button>
-                    <OpenModalButton
-                      buttonText="Update Button"
-                      modalComponent={<UpdatePost post={post} />}
-                    />
-                  </>
-                ) : (
-                  <>
-                    <button>Like</button>
-                    <button>Comment</button>
-                  </>
-                )}
-
-                {/* <OpenModalButton
-                buttonText="Update Button"
-                modalComponent={<UpdatePost id={post?.id} />}
-              /> */}
-              </div>
-            </div>
-          ))}
+          <UserPost />
         </div>
       </div>
-      <div style={{ backgroundColor: 'red' }}>
+      <div style={{ backgroundColor: '#8ecae6' }}>
         <h2>FOLLOWERS/FOLLOWING</h2>
       </div>
+
+      {/* {currentUser && (
+        <>
+          <button onClick={() => dispatch(deletePost(post?.id))}>Delete Post</button>
+          <OpenModalButton
+            buttonText="Update Button"
+            modalComponent={<UpdatePost post={post} />}
+          />
+        </>
+      )} */}
     </div>
   );
 }
