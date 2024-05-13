b25c4ab0273e16719803d60e26217ecaf10ec17c
Oscar Robles <oscarrsvp@gmail.com>
2024-05-12 23:07:40 -0500

    Refactor component & added styles

--- frontend/src/components/Homepage/CommentSection.jsx
+++ frontend/src/components/Homepage/CommentSection.jsx
@@ -1,13 +1,17 @@
-import { useDispatch, useSelector } from 'react-redux';
 import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AiOutlineLike } from 'react-icons/ai';
+import { FaRegComment } from 'react-icons/fa';
 import { createComment, deleteComment } from '../../store/commentSlice';
 import OpenModalButton from '../OpenModalButton/OpenModalButton';
 import UpdateComment from '../UpdateInputs/UpdateComment';
+import styles from './Homepage.module.css';
 
 function CommentSection({ post, comments }) {
   const postId = post.id;
   const [comment, setComment] = useState('');
   const [showComments, setShowComments] = useState(false);
+  const [newComment, setNewComment] = useState(false);
   const sessionUser = useSelector((state) => state.session.user);
   const dispatch = useDispatch();
 
@@ -16,34 +20,58 @@ function CommentSection({ post, comments }) {
 
     dispatch(createComment({ context: comment, postId }));
     setComment('');
+    setNewComment(!newComment);
+    setShowComments(!showComments);
   };
 
   return (
     <div>
-      <h3>Comments</h3>
-      <div>
-        <input type="text" value={comment} onChange={(e) => setComment(e.target.value)} />
+      {newComment && (
+        <div className={styles.newComment}>
+          <textarea
+            type="text"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+          />
+          <button onClick={handleComment}>Add new comment</button>
+        </div>
+      )}
 
-        <button>Like</button>
-        <button onClick={handleComment}>Comment</button>
+      <div className="flexBetween">
+        <div>
+          <AiOutlineLike cursor={'pointer'} size={20} className="icons" />
+          <FaRegComment
+            cursor={'pointer'}
+            size={20}
+            onClick={() => setNewComment(!newComment)}
+          />
+        </div>
+
+        <p className="viewComments" onClick={() => setShowComments(!showComments)}>
+          {!showComments ? `View all ${post.numOfComments} comments` : 'Hide Comments'}
+        </p>
       </div>
-      <button onClick={() => setShowComments(!showComments)}>
-        {!showComments ? 'View Comments' : 'Hide Comments'}
-      </button>
 
       {showComments && (
-        <div>
+        <div className={styles.commentSection}>
           {comments.map((comment) => (
-            <div key={comment?.id}>
-              <p>{comment?.context}</p>
-              {sessionUser.id === comment?.userId && (
+            <div key={comment.id} className={styles.postComments}>
+              <div className="flexBetween">
+                <div className="flexBetween">
+                  <p className={styles.userImage}></p>
+                  <h4>{comment.fullName}</h4>
+                </div>
+                <h5>Posted: {comment.createdAt}</h5>
+              </div>
+              <p className={styles.comments}>{comment.context}</p>
+              {sessionUser.id === comment.userId && (
                 <div>
                   <OpenModalButton
                     buttonText="Edit Comment"
                     modalComponent={<UpdateComment comment={comment} />}
                   />
                   <button
-                    onClick={() => dispatch(deleteComment({ postId, id: comment?.id }))}
+                    onClick={() => dispatch(deleteComment({ postId, id: comment.id }))}
                   >
                     Delete
                   </button>
