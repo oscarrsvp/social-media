22363998f02829a582743254b30228ecae4040b8
Oscar Robles <oscarrsvp@gmail.com>
2024-05-12 23:08:35 -0500

    Refactor component included icons along with styles

--- frontend/src/components/Homepage/UserPost.jsx
+++ frontend/src/components/Homepage/UserPost.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from 'react-redux';
+import { BsThreeDotsVertical } from 'react-icons/bs';
 import CommentSection from './CommentSection';
 import styles from './Homepage.module.css';
 
@@ -12,6 +13,8 @@ function UserPost() {
       day: 'numeric',
       month: 'long',
       year: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
     });
 
     return dates;
@@ -23,20 +26,34 @@ function UserPost() {
     <>
       {userPost.map((user) => (
         <div className={styles.userPost} key={user.id}>
-          <div>
-            <h2>
-              {user.User.firstName} {user.User.lastName}
-            </h2>
-            <h3></h3>
-            <h4>{createdAt(user.createdAt)}</h4>
+          <div className="flexBetween p-16">
+            <div className="flexBetween">
+              <p className={styles.userImage}></p>
+              <h3>
+                {user.User.firstName} {user.User.lastName}
+              </h3>
+            </div>
+            <BsThreeDotsVertical cursor={'pointer'} size={20} />
           </div>
 
-          <div>
-            <img src={user.photo} />
-            <p>{user.context}</p>
-          </div>
+          <img src={user.photo} />
 
-          <CommentSection post={user} comments={user.Comments} />
+          <div className={styles.postDetails}>
+            <div>
+              <p>
+                <span>
+                  {user.User.firstName} {user.User.lastName}
+                </span>
+                {user.context}
+              </p>
+            </div>
+
+            <h5>Posted: {createdAt(user.createdAt)}</h5>
+
+            <div>
+              <CommentSection post={user} comments={user.Comments} />
+            </div>
+          </div>
         </div>
       ))}
     </>
