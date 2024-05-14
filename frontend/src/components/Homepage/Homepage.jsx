82efeef4a71f26e75aa8270fa3557f4ffc3cf951
Oscar Robles <oscarrsvp@gmail.com>
2024-05-13 23:00:42 -0500

    Refactor broken into smaller components

--- frontend/src/components/Homepage/Homepage.jsx
+++ frontend/src/components/Homepage/Homepage.jsx
@@ -1,29 +1,28 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Navigate } from 'react-router-dom';
-import { fetchPosts, createPost, deletePost } from '../../store/postSlice';
-import OpenModalButton from '../OpenModalButton/OpenModalButton';
-import UpdatePost from '../UpdateInputs/UpdatePost';
+import { fetchPosts } from '../../store/postSlice';
+import { fetchUsers } from '../../store/userSlice';
+import CreatePost from '../PostForm/CreatePost';
 import UserPost from './UserPost';
 import styles from './Homepage.module.css';
 
 function Homepage() {
-  const [photo, setPhoto] = useState('');
-  const [context, setContext] = useState('');
   const sessionUser = useSelector((state) => state.session.user);
+  const posts = useSelector((state) => state.posts);
+  const userPost = Object.values(posts);
   const dispatch = useDispatch();
+  const fullName = `${sessionUser.firstName} ${sessionUser.lastName}`;
 
   useEffect(() => {
     dispatch(fetchPosts());
+
+    dispatch(fetchUsers());
   }, [dispatch]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    dispatch(createPost({ photo, context }));
-  };
-
   if (!sessionUser) return <Navigate to="/" replace={true} />;
+
+  if (posts.post === null) return <h1>Loading</h1>;
 
   return (
     <div id={styles.homePage}>
@@ -34,42 +33,17 @@ function Homepage() {
         <div>Message -- Link to chat page</div>
       </div>
       <div className={styles.feedContainer}>
-        {/* <div>HEADERS</div> */}
         <div className={styles.feed}>
-          <h2>POST DETAILS</h2>
-          <form onSubmit={handleSubmit}>
-            <label>
-              <input
-                type="text"
-                placeholder="Photo"
-                value={photo}
-                onChange={(e) => setPhoto(e.target.value)}
-              />
-            </label>
-            <label>
-              <textarea
-                value={context}
-                onChange={(e) => setContext(e.target.value)}
-              ></textarea>
-            </label>
-            <button type="submit">Create New Post</button>
-          </form>
-          <UserPost />
+          <h2>Welcome, {fullName}</h2>
+          <CreatePost />
+          {userPost.map((post) => (
+            <UserPost post={post} userId={post.userId} key={post.id} />
+          ))}
         </div>
       </div>
       <div style={{ backgroundColor: '#8ecae6' }}>
         <h2>FOLLOWERS/FOLLOWING</h2>
       </div>
-
-      {/* {currentUser && (
-        <>
-          <button onClick={() => dispatch(deletePost(post?.id))}>Delete Post</button>
-          <OpenModalButton
-            buttonText="Update Button"
-            modalComponent={<UpdatePost post={post} />}
-          />
-        </>
-      )} */}
     </div>
   );
 }
