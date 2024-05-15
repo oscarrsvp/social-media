bed3dcc31fde4d6137674590afa03796837897db
Oscar Robles <oscarrsvp@gmail.com>
2024-05-14 20:08:05 -0500

    Modify the / route

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
-const { Op } = require('sequelize');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { validateSignup } = require('../../utils/validation');
 const { User, Post } = require('../../db/models');
@@ -9,15 +8,13 @@ const router = express.Router();
 
 // Get User's Information
 router.get('/', requireAuth, async (req, res) => {
-  const userId = req.user.id;
-
   const user = await User.findAll({
-    include: Post,
-    where: {
-      id: {
-        [Op.ne]: userId,
+    include: [
+      {
+        model: Post,
+        order: [['createdAt', 'DESC']],
       },
-    },
+    ],
   });
 
   return res.json({ User: user });
