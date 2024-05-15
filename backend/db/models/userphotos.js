4541c9110802ae8019eae48e4e533bb452414574
Oscar Robles <oscarrsvp@gmail.com>
2024-05-14 22:15:27 -0500

    Added UserPhotos Model/Seeders

---
+++ backend/db/models/userphotos.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class UserPhoto extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      UserPhoto.belongsTo(models.User, {
+        foreignKey: 'userId',
+      });
+    }
+  }
+  UserPhoto.init(
+    {
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      url: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      preview: {
+        type: DataTypes.BOOLEAN,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'UserPhoto',
+    },
+  );
+  return UserPhoto;
+};
