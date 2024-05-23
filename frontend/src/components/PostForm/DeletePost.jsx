d8f05970b10449151d2f81741185dcd8d002073c
Oscar Robles <oscarrsvp@gmail.com>
2024-05-23 18:29:09 -0500

    Refactor component to let user confirm to delete post

--- frontend/src/components/PostForm/DeletePost.jsx
+++ frontend/src/components/PostForm/DeletePost.jsx
@@ -1,15 +1,32 @@
 import { useDispatch, useSelector } from 'react-redux';
+import { useModal } from '../../context/Modal';
 import { deletePost } from '../../store/postSlice';
 
 function DeletePost({ postId, userId }) {
   const sessionUser = useSelector((state) => state.session.user);
+  const { closeModal } = useModal();
   const dispatch = useDispatch();
+
+  const handleDelete = () => {
+    dispatch(deletePost(postId));
+    closeModal();
+  };
+
   return (
     <>
       {sessionUser.id === userId && (
-        <button className="btn delete-btn" onClick={() => dispatch(deletePost(postId))}>
-          Delete Post
-        </button>
+        <div className="deleteModal">
+          <h1>Confirm Delete?</h1>
+          <p>Are you sure you want to delete this post?</p>
+
+          <button className="btn delete-btn" onClick={handleDelete}>
+            Delete Post
+          </button>
+
+          <button className="btn" onClick={closeModal}>
+            No (Keep Post)
+          </button>
+        </div>
       )}
     </>
   );
