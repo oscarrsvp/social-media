d1135d0e49f2fc51a1cada482042a408c8395bef
Oscar Robles <oscarrsvp@gmail.com>
2024-05-27 16:32:42 -0500

    Handles displaying the users name and image

--- frontend/src/components/Homepage/DisplayName.jsx
+++ frontend/src/components/Homepage/DisplayName.jsx
@@ -4,7 +4,12 @@ function DisplayName() {
   const sessionUser = useSelector((state) => state.session.user);
   const fullName = `${sessionUser.firstName} ${sessionUser.lastName}`;
 
-  return <h3>{fullName}</h3>;
+  return (
+    <div className="center m-8">
+      <img className="profileImg" src={sessionUser.profileImage} alt="user-image" />
+      <h3>{fullName}</h3>
+    </div>
+  );
 }
 
 export default DisplayName;
