f3d7e5f9cbda1af5e6780c09717bf321c07bb4c1
Oscar Robles <oscarrsvp@gmail.com>
2024-06-30 15:47:04 -0500

    Users can now Add/Edit their profile picture

--- frontend/src/components/UserPage/UserCard.jsx
+++ frontend/src/components/UserPage/UserCard.jsx
@@ -1,24 +1,28 @@
+import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { SlUser } from 'react-icons/sl';
 import { followUser, unfollowUser } from '../../store/followSlice';
-// import { updateUser } from '../../store/userSlice';
-// import { updateProfileImage } from '../../store/sessionSlice';
-import { featureComingSoon } from '../../utils/globallyFns';
+import { updateUser } from '../../store/userSlice';
+import { updateProfileImage } from '../../store/sessionSlice';
 import styles from './UserPage.module.css';
 
 function UserCard({ user }) {
+  const [isActive, setIsActive] = useState(false);
+  const [profileImg, setProfileImg] = useState('' || user.profileImage);
   const sessionUser = useSelector((state) => state.session.user);
   const followingList = useSelector((state) => state.following);
   const fullName = `${user.firstName} ${user.lastName}`;
   const dispatch = useDispatch();
 
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { firstName, lastName } = user;
 
-  //   dispatch(updateProfileImage(profileImg));
-
-  //   return dispatch(updateUser({ profileImage: profileImg }));
-  // };
+    dispatch(updateProfileImage(profileImg));
+    dispatch(updateUser({ profileImage: profileImg, firstName, lastName }));
+    setIsActive(!isActive);
+    return;
+  };
 
   return (
     <div className={styles.userActions}>
@@ -33,6 +37,18 @@ function UserCard({ user }) {
         <h2>{fullName}</h2>
         <p>Message</p>
         <p>Photos</p>
+        {isActive ? (
+          <>
+            <input
+              type="text"
+              name="profileImg"
+              id="profileImg"
+              value={profileImg}
+              placeholder="Enter Image URL"
+              onChange={(e) => setProfileImg(e.target.value)}
+            />
+          </>
+        ) : null}
       </div>
 
       {sessionUser.id !== user.id ? (
@@ -48,9 +64,23 @@ function UserCard({ user }) {
           )}
         </div>
       ) : (
-        <button className="btn" onClick={(e) => featureComingSoon(e)}>
-          Upload Picture
-        </button>
+        <>
+          {isActive ? (
+            <div>
+              <button className="btn" onClick={(e) => handleSubmit(e)}>
+                Edit Picture
+              </button>
+
+              <button className="btn" onClick={() => setIsActive((prev) => !prev)}>
+                Cancel
+              </button>
+            </div>
+          ) : (
+            <button className="btn" onClick={() => setIsActive((prev) => !prev)}>
+              {user.profileImage ? 'Change Picture' : 'Upload Picture'}
+            </button>
+          )}
+        </>
       )}
     </div>
   );
