1810cdf5c1a8ee572314085afec086b113ef970e
Oscar Robles <oscarrsvp@gmail.com>
2024-09-19 22:33:56 -0500

    cloudinary upload setup complete

--- backend/cloudinary.js
+++ backend/cloudinary.js
@@ -1,13 +1,37 @@
 require('dotenv').config();
 
 const cloudinary = require('cloudinary').v2;
+const path = require('path');
+const multer = require('multer');
+const DatauriParser = require('datauri/parser');
+
+const parser = new DatauriParser();
 
 // Configuration
-
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-module.exports = cloudinary;
+const formatBufferTo64 = (file) =>
+  parser.format(path.extname(file.originalname).toString(), file.buffer);
+
+const singlePublicFileUpload = async (file) => {
+  console.log('file', file);
+  const data = formatBufferTo64(file);
+  return await cloudinary.uploader.upload(data.content);
+};
+
+const storage = multer.memoryStorage({
+  destination: function (_req, _file, callback) {
+    callback(null, '');
+  },
+});
+
+const singleMulterUpload = (nameOfKey) => multer({ storage: storage }).single(nameOfKey);
+
+module.exports = {
+  singlePublicFileUpload,
+  singleMulterUpload,
+};
