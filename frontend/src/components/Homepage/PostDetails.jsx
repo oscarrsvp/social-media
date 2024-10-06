c81653fe6dae80caee9e99855e6237e9f7157c9b
Oscar Robles <oscarrsvp@gmail.com>
2024-10-06 14:56:28 -0500

    refactor: removed onClick function and flow of component

--- frontend/src/components/Homepage/PostDetails.jsx
+++ frontend/src/components/Homepage/PostDetails.jsx
@@ -1,30 +1,21 @@
-import { useState } from 'react';
 import { createdAt } from '../../utils/globallyFns';
 import styles from './Homepage.module.css';
 
 function PostDetails({ post, fullName }) {
   const postImage = !post.photo ? '' : post.photo;
-  const [fullImage, setFullImage] = useState(false);
-
-  const toggleImage = fullImage ? styles.fullImage : styles.postImage;
 
   return (
     <div className={styles.userDetails}>
-      {postImage ? (
-        <>
-          <div onClick={() => setFullImage(!fullImage)}>
-            <img src={post.photo} className={toggleImage} />
-          </div>
-          <p>
-            <span>{fullName}</span>
-            {post.context}
-          </p>
-        </>
-      ) : (
-        <p>{post.context}</p>
-      )}
+      {postImage && <img src={post.photo} alt="post-image" />}
 
-      {<h5>Posted: {createdAt(post.createdAt)}</h5>}
+      <div className={styles.postDescription}>
+        <p>
+          {postImage && <span>{fullName}</span>}
+          {post.context}
+        </p>
+
+        {<h5>Posted: {createdAt(post.createdAt)}</h5>}
+      </div>
     </div>
   );
 }
