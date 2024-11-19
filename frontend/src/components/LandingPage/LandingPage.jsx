a8f984150ffa83c97b9fd3a0c83c6b510a1d6cec
Oscar Robles <oscarrsvp@gmail.com>
2024-11-18 22:26:28 -0600

    style(login): implement mobile responsiveness for login form

--- frontend/src/components/LandingPage/LandingPage.jsx
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -1,23 +1,35 @@
+import { useContext } from 'react';
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
+import { MobileContext } from '../../App';
 import LoginForm from '../LoginForm/LoginForm';
 import styles from './LandingPage.module.css';
 import socialGathering from '../../assets/friends-gathering.jpeg';
 
 function LandingPage() {
   const sessionUser = useSelector((state) => state.session.user);
+  const isMobile = useContext(MobileContext);
 
   if (sessionUser) return <Navigate to="/homepage" replace={true} />;
 
   return (
     <div className={styles.landingPage}>
-      <div className={`flex ${styles.sections}`}>
-        <div className={styles.heading}>
-          <h1>Bring Moments to Life: Share with Family and Friends</h1>
+      {isMobile && (
+        <div className={styles.header}>
+          <h1>Connect and Discover</h1>
+          <span>Endless possibilities await.</span>
         </div>
-        <img src={socialGathering} alt="friends-gathering" srcSet="" />
-      </div>
-      <div className={`flex ${styles.sections}`}>
+      )}
+
+      {!isMobile && (
+        <div className={`flex ${styles.sections}`}>
+          <div className={styles.heading}>
+            <h1>Bring Moments to Life: Share with Family and Friends</h1>
+          </div>
+          <img src={socialGathering} alt="friends-gathering" srcSet="" />
+        </div>
+      )}
+      <div id={styles.sectionForm} className={`flex ${isMobile ? null : styles.section}`}>
         <LoginForm />
       </div>
     </div>
