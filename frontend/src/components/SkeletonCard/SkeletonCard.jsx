56982d9668b8df07af41096a3b537d86959d9606
Oscar Robles <oscarrsvp@gmail.com>
2024-11-30 07:37:41 -0600

    style: add skeletonCard module file for styles

--- frontend/src/components/SkeletonCard/SkeletonCard.jsx
+++ frontend/src/components/SkeletonCard/SkeletonCard.jsx
@@ -1,20 +1,21 @@
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
+import styles from './SkeletonCard.module.css';
 
 function SkeletonCard({ length }) {
   return Array(length)
     .fill(0)
     .map((_, index) => (
-      <div className="skeleton-card" key={index}>
-        <div className="post-header">
+      <div className={styles.skeletonCard} key={index}>
+        <div className={styles.postHeader}>
           <Skeleton circle height={'40px'} width={'40px'} />
           <Skeleton width={'150px'} />
         </div>
-        <div className="post-details">
+        <div className={styles.postDetails}>
           <Skeleton count={3} />
         </div>
 
-        <div className="comment-section">
+        <div className={styles.commentSection}>
           <Skeleton circle height={'30px'} width={'30px'} />
           <Skeleton height={'60px'} width={'680px'} />
         </div>
--- frontend/src/components/SkeletonCard/SkeletonFollowingSection.jsx
+++ frontend/src/components/SkeletonCard/SkeletonFollowingSection.jsx
@@ -1,17 +1,18 @@
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
+import styles from './SkeletonCard.module.css';
 
 function SkeletonFollowingSection() {
   return (
-    <div className="skeleton-follow-section">
-      <div className="follow-header">
+    <div className={styles.skeletonFollowSection}>
+      <div className={styles.followHeader}>
         <Skeleton width={'150px'} />
       </div>
 
       {Array(2)
         .fill(0)
         .map((_, index) => (
-          <div className="following" key={index}>
+          <div className={styles.following} key={index}>
             <Skeleton circle height={'40px'} width={'40px'} />
             <Skeleton width={'180px'} />
           </div>
