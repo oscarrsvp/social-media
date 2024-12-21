96397fa191abaea3d202d2b0453e3492cdb50dc3
Oscar Robles <oscarrsvp@gmail.com>
2024-12-21 14:30:31 -0600

    style: add styles to component

--- frontend/src/components/UserPhotos/UserPhotos.jsx
+++ frontend/src/components/UserPhotos/UserPhotos.jsx
@@ -1,21 +1,28 @@
 import { useSelector } from 'react-redux';
+import styles from './UserPhotos.module.css';
 
 function UserPhotos() {
   const userPhotos = useSelector((state) => state.userPhotos);
+  const firstName = useSelector((state) => state.session.user.firstName);
   const photos = Object.values(userPhotos);
 
   return (
-    <>
-      <h1>List of Photos</h1>
-      {photos.map((photo) => (
-        <img
-          src={photo.url}
-          alt=""
-          key={photo.id}
-          style={{ width: '100px', height: '100px' }}
-        />
-      ))}
-    </>
+    <div>
+      {photos.length === 0 ? (
+        <div className="noContent">
+          <h1>{firstName} doesn&apos;t have any photos</h1>
+        </div>
+      ) : (
+        <>
+          <h1>{firstName}&apos;s Photos</h1>
+          <div className={styles.userPictures}>
+            {photos.map((photo) => (
+              <img src={photo?.url} key={photo?.id} />
+            ))}
+          </div>
+        </>
+      )}
+    </div>
   );
 }
 
