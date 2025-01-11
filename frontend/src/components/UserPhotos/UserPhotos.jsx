2ccfff20f4656c3057df15d73a0dd07c51af4442
Oscar Robles <oscarrsvp@gmail.com>
2025-01-11 10:13:46 -0600

    feat(ui): add useState to display user photos on span click

--- frontend/src/components/UserPhotos/UserPhotos.jsx
+++ frontend/src/components/UserPhotos/UserPhotos.jsx
@@ -1,10 +1,14 @@
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import styles from './UserPhotos.module.css';
 
 function UserPhotos() {
   const userPhotos = useSelector((state) => state.userPhotos);
   const firstName = useSelector((state) => state.session.user.firstName);
+  const [showAllPhotos, setShowAllPhotos] = useState(false);
   const photos = Object.values(userPhotos);
+
+  const displayedPhotos = showAllPhotos ? photos : photos.slice(0, 3);
 
   return (
     <div id={styles.userPhotosSection}>
@@ -14,9 +18,14 @@ function UserPhotos() {
         </div>
       ) : (
         <>
-          <h2>Photos</h2>
+          <div className={styles.photoSection}>
+            <h2>Photos</h2>
+            {photos.length > 3 && !showAllPhotos && (
+              <span onClick={() => setShowAllPhotos(true)}>See all photos</span>
+            )}
+          </div>
           <div className={styles.userPictures}>
-            {photos.map((photo) => (
+            {displayedPhotos.map((photo) => (
               <img src={photo?.url} key={photo?.id} />
             ))}
           </div>
