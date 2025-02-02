70cafaed7212a31b43b97e3bbce9b5fc74ef1383
Oscar Robles <oscarrsvp@gmail.com>
2025-02-01 18:46:32 -0600

    feat: use useSelector to fetch search results from Redux store

--- frontend/src/components/Navigation/SearchBar.jsx
+++ frontend/src/components/Navigation/SearchBar.jsx
@@ -1,30 +1,53 @@
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useState } from 'react';
 import { IoSearchOutline } from 'react-icons/io5';
 import { searchUsers } from '../../store/searchBarSlice';
 
 function SearchBar() {
+  const searchResults = useSelector((state) => state.searchResults);
   const [search, setSearch] = useState('');
+  const results = Object.values(searchResults);
   const dispatch = useDispatch();
 
   const handleSearchChange = (e) => {
     const searchValue = e.target.value;
     setSearch(searchValue);
     dispatch(searchUsers(searchValue));
+
+    if (searchValue.trim() === '') {
+      dispatch(searchUsers(null));
+    } else {
+      dispatch(searchUsers(searchValue));
+    }
   };
 
   return (
-    <div className="search-section">
-      <input
-        type="search"
-        name="search"
-        id="search-bar"
-        value={search}
-        onChange={handleSearchChange}
-        placeholder="Search for a user..."
-      />
-      <IoSearchOutline className="search-icon" />
-    </div>
+    <>
+      <div className="search-section">
+        <input
+          type="search"
+          name="search"
+          id="search-bar"
+          value={search}
+          onChange={handleSearchChange}
+          placeholder="Search for a user..."
+        />
+        <IoSearchOutline className="search-icon" />
+      </div>
+
+      {searchResults.search !== null && results !== 0 && (
+        <div>
+          {results.map((users) => (
+            <div key={`user${users?.id}`}>
+              <img src={users?.profileImg} alt="" />
+              <h1>
+                {users?.firstName} {users?.lastName}
+              </h1>
+            </div>
+          ))}
+        </div>
+      )}
+    </>
   );
 }
 
