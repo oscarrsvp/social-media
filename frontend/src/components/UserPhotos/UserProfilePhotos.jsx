02dcc06dcf5e39a43e3d7d600d9488fee4ee479e
Oscar Robles <oscarrsvp@gmail.com>
2025-03-24 12:26:20 -0500

    style(ui): improve photo comment section layout and styling

--- frontend/src/components/UserPhotos/UserProfilePhotos.jsx
+++ frontend/src/components/UserPhotos/UserProfilePhotos.jsx
@@ -1,14 +1,18 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 import { useModal } from '../../context/Modal';
 import { VscChromeClose } from 'react-icons/vsc';
 import { fetchPhotoData } from '../../store/photoCommentSlice';
+import { formatDate } from '../../utils/globallyFns';
 import PhotoCommentSection from './photoCommentSection';
+import BlankImage from '../../assets/blank-profile-picture.png';
 import styles from './UserPhotos.module.css';
 
 function UserProfilePhotos({ photo }) {
   const { closeModal } = useModal();
   const photosComments = useSelector((state) => state.photoComments);
+  const userProfile = useSelector((state) => state.users[photo?.userId]);
   const photoData = Object.values(photosComments[photo?.id] || []);
   const dispatch = useDispatch();
 
@@ -23,18 +27,36 @@ function UserProfilePhotos({ photo }) {
       <VscChromeClose onClick={closeModal} className={styles.closeIcon} size={25} />
       <div className={styles.singlePhoto}>
         <img src={photo.url} alt="" />
-        {photoData.length > 1 ? (
-          <div className={styles.commentSection}>
-            {photoData.map((comments) => (
-              <PhotoCommentSection
-                data={comments}
-                key={`userComments-${comments.userId}`}
-              />
-            ))}
+        <div className={styles.commentSection}>
+          <div className={styles.header}>
+            <div className={styles.userImage}>
+              {userProfile.profileImage ? (
+                <img src={userProfile.profileImage} alt="" />
+              ) : (
+                <img src={BlankImage} />
+              )}
+
+              <NavLink to={`/user/${userProfile.id}`}>
+                {userProfile.firstName} {userProfile.lastName}
+              </NavLink>
+            </div>
+
+            <p>Posted: {formatDate(userProfile.createdAt)}</p>
           </div>
-        ) : (
-          <p>No Comments yet...</p>
-        )}
+
+          {photoData.length > 1 ? (
+            <div className={styles.userComments}>
+              {photoData.map((comments) => (
+                <PhotoCommentSection
+                  data={comments}
+                  key={`userComments-${comments.userId}`}
+                />
+              ))}
+            </div>
+          ) : (
+            <p className={styles.noComments}>Be the first to leave a comment!</p>
+          )}
+        </div>
       </div>
     </div>
   );
