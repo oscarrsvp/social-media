d1dcc9a82e10c4d806478da8a8c10c53ed16c391
Oscar Robles <oscarrsvp@gmail.com>
2025-03-27 13:11:46 -0500

    feat: add component to allow user to update photo comments

---
+++ frontend/src/components/UpdateInputs/UpdatePhotoComment.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useModal } from '../../context/Modal';
+import { VscChromeClose } from 'react-icons/vsc';
+import { EditPhotoComment } from '../../store/photoCommentSlice';
+import DisplayName from '../Homepage/DisplayName';
+import styles from './UpdateInputs.module.css';
+
+function UpdatePhotoComment({ comment }) {
+  const { id } = comment;
+  const [context, setContext] = useState(comment.context || '');
+  const [errors, setErrors] = useState({});
+  const { closeModal } = useModal();
+  const dispatch = useDispatch();
+
+  const handleUpdate = () => {
+    const editComment = dispatch(EditPhotoComment({ id, context }));
+
+    return editComment.then(async (res) => {
+      const data = await res;
+      if (data.error) {
+        setErrors(data.payload);
+      } else {
+        setContext('');
+        closeModal();
+      }
+    });
+  };
+
+  return (
+    <>
+      <div>
+        <div className={`${styles.header} p-15 border-line`}>
+          <DisplayName />
+          <VscChromeClose onClick={closeModal} />
+        </div>
+
+        <div className="p-15">
+          <textarea
+            value={context}
+            onChange={(e) => setContext(e.target.value)}
+            placeholder="Whats on your mind?"
+            autoFocus
+          ></textarea>
+          {/* {errors.context && <p className="error">{errors.context}</p>} */}
+          <button className="btn update-btn" onClick={handleUpdate}>
+            Save
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default UpdatePhotoComment;
